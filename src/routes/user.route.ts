49ca4de73e3f250d2c3addd4faba453596859c30
Angel Reyes <a22110083@ceti.mx>
2024-02-29 08:38:25 -0600

    Actividad 3 Terminada - Autenticacion JWT - Proteccion de Rutas - Ocultar Password al Cliente

--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -8,7 +8,6 @@ const service = new UserService()
 
 router.post('/', async (req, res, next) => {
   try {
-    //TODO: Validate user data coming from the request
     const user: User = req.body
     const newUser = await service.create(user)
     res.status(201).json({ user: newUser.toClient() })
